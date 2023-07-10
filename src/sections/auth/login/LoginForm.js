904baa307e96c4a1a19f7a21350619c8d9619a53
Seyed Mohammad Mahdi Hatami <hatamik7@gmail.com>
2023-07-10 10:41:23 +0330

    Login api connected, authentication remains

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -35,24 +35,45 @@ export default function LoginForm() {
     pass: 'invalid password'
   };
 
-  const handleSubmit = (event) => {
+  const [uname, setName] = useState('')
+  const [pass, setPassword] = useState('')
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const { uname, pass } = event.target.elements;
+    const response = await fetch("http://localhost:8080/login/", {
+      method: "POST", 
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Origin': 'http://localhost:3000'
+      },
+      // credentials:'include',
+      body: JSON.stringify({
+        "username": uname,
+        "password": pass
+      })
+    })
 
-    const userData = database.find((user) => user.username === uname.value);
-
-    if (userData) {
-      if (userData.password !== pass.value) {
-        setErrorMessages({ name: 'pass', message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-        account.displayName = userData.username;
-        account.email = userData.email;
-      }
-    } else {
-      setErrorMessages({ name: 'uname', message: errors.uname });
+    const content = await response.json()
+    if (response.ok) {
+      setIsSubmitted(true);
+      account.displayName = content.username;
+      // account.email = userData.email;
     }
+
+    // const userData = database.find((user) => user.username === uname.value);
+
+    // if (userData) {
+    //   if (userData.password !== pass.value) {
+    //     setErrorMessages({ name: 'pass', message: errors.pass });
+    //   } else {
+    //     setIsSubmitted(true);
+    //     account.displayName = userData.username;
+    //     account.email = userData.email;
+    //   }
+    // } else {
+    //   setErrorMessages({ name: 'uname', message: errors.uname });
+    // }
   };
 
   const renderErrorMessage = (name) =>
@@ -65,13 +86,15 @@ export default function LoginForm() {
       <form onSubmit={handleSubmit}>
         <div className="input-container">
         <label htmlFor="btn-check5" className="btn btn-primary-border">
-            <input placeholder='email' type="text" name="uname" required />
+            <input placeholder='email' type="text" name="uname" required
+              onChange={e => setName(e.target.value)} />
           </label>
           {renderErrorMessage('uname')}
         </div>
         <div className="input-container">
         <label htmlFor="btn-check5" className="btn btn-primary-border">
-            <input placeholder="password" type="password" name="pass" required />
+            <input placeholder="password" type="password" name="pass" required
+              onChange={e => setPassword(e.target.value)} />
           </label>
           {renderErrorMessage('pass')}
         </div>
